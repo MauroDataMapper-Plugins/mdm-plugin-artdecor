91c3fa2831c29c4cda27218f862f3ac77815cb34
Ollie Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-01-28 15:52:25 +0000

    Update LICENSE and NOTICE with the copyright to include NHSD

--- grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/artdecor/ArtDecorDataModelImporterProviderService.groovy
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/artdecor/ArtDecorDataModelImporterProviderService.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 University of Oxford
+ * Copyright 2020 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,14 @@
  */
 package uk.ac.ox.softeng.maurodatamapper.plugins.artdecor
 
-import org.springframework.beans.factory.annotation.Autowired
 import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiUnauthorizedException
 import uk.ac.ox.softeng.maurodatamapper.datamodel.DataModel
 import uk.ac.ox.softeng.maurodatamapper.datamodel.DataModelService
 import uk.ac.ox.softeng.maurodatamapper.datamodel.provider.importer.DataModelImporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.security.User
+
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 
 @Slf4j
 class ArtDecorDataModelImporterProviderService<T extends ArtDecorDataModelImporterProviderServiceParameters>
